9bf93b5c199ffee24d7d864b50680aafc78d2e0d
John Heng <hengj@vcu.edu>
2017-11-10 15:22:07 -0500

    Import material library

---
+++ sport-stats/src/app/stat.component.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { Component, NgModule } from '@angular/core';
+
+
+@Component({
+  selector: 'stat',
+  templateUrl: './stat.component.html',
+  styleUrls: ['./stat.component.css']
+})
+
+
+export class StatComponent {
+  title = 'app';
+}
